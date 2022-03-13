2dd48ac425e5707536ce0d4b4bda158edd7ac7b3
Chirag <chiragparchani1234@gmail.com>
2022-03-13 15:42:54 +0530

    Images added

--- server.js
+++ server.js
@@ -148,7 +148,11 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Server is running on Port 3000");
+    app.listen(8000, () => {
+      console.log("Server is running on Port 8000");
     });
+  })
+  .catch((e) => {
+    console.log(e)
+
   });
