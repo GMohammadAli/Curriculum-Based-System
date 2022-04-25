8f8e826dc3b3ae6e130137e6594055e58a631443
GMohammadAli <gulammohammadali@gmail.com>
2022-04-25 15:48:11 +0530

    Heroku Deployment

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    secure: true ,
+    // secure: true ,
     // says that the cookie can be configured only when used through https request
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7
