7182b77991bb8e418c1e7c2ee54e0fe284e8775b
GMohammadAli <gulammohammadali@gmail.com>
2022-07-01 14:58:56 +0530

    Updated Filter Function again

--- controllers/courses.js
+++ controllers/courses.js
@@ -94,9 +94,8 @@ async function runFilter(_filter, value) {
   const database = client.db("CBS");
   const collections = database.collection("courses");
   console.log(_filter + " in filter funtion");
-  //if (_filter === "domain") {
-    const q = _filter;
-    const query = { q : `${value}` };
+    //const q = ;
+    const query = { _filter : `${value}` };
     const courses = await collections.find(query);
     const coursesArray = await courses
       .map(function (course) {
@@ -105,25 +104,4 @@ async function runFilter(_filter, value) {
       .toArray();
     console.log(coursesArray);
     return coursesArray;
-  // } else if (_filter === "price") {
-  //   const query = { price: `${value}` };
-  //   const courses = await collections.find(query);
-  //   const coursesArray = await courses
-  //     .map(function (course) {
-  //       return course;
-  //     })
-  //     .toArray();
-  //   console.log(coursesArray);
-  //   return coursesArray;
-  // } else if (_filter === "platform") {
-  //   const query = { platform: `${value}` };
-  //   const courses = await collections.find(query);
-  //   const coursesArray = await courses
-  //     .map(function (course) {
-  //       return course;
-  //     })
-  //     .toArray();
-  //   console.log(coursesArray);
-  //   return coursesArray;
-  // }
 }
